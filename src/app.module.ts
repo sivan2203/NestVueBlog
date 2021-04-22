1db8053e7c68f7a7c392132ef62b2e87e16e04b6
sivan2203 <sivan9458@gmail.com>
2021-04-22 16:59:40 +0700

    fix bug in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from "@nestjs/mongoose";
-import { BlogModule } from './blog/blog.module';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BlogModule } from './blog/blog.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest-blog', { useNewUrlParser: true }),
+    MongooseModule.forRoot('mongodb://localhost/nest-blog', {
+      useNewUrlParser: true,
+    }),
     BlogModule,
   ],
   controllers: [AppController],
